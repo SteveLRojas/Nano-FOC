2ece56e4204899ad98eb464bb1cf63e9235191b3
SteveLRojas <looserr2@illinois.edu>
2026-02-28 12:03:10 +0100

    Parameterized balanced_clarke module

--- Nano-FOC_LandShark_ADC_Bridge/src/balanced_clarke.v
+++ Nano-FOC_LandShark_ADC_Bridge/src/balanced_clarke.v
@@ -1,12 +1,38 @@
-module balanced_clarke(
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer: Esteban Looser-Rojas (ELR)
+// 
+// Create Date:    14:10:50 03/21/2023
+// Design Name:    balanced_clarke
+// Module Name:    balanced_clarke
+// Project Name:   Nano-FOC
+// Target Devices: Cyclone IV, Cyclone 10, MAX 10
+// Tool versions:  ISE 14.7, Quartus 19.1 Lite
+// Description:    Balanced Clarke transformation.
+//
+// Dependencies: None.
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Revision 1.00 - Design completed.
+// Revision 1.10 - Design parameterized.
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module balanced_clarke
+	#(
+		parameter NUM_BITS = 14
+	)
+	(
 		input wire clk,
 		input wire rst,
 		input trig,
-		input wire signed[13:0] in_u,
-		input wire signed[13:0] in_v,
-		input wire signed[13:0] in_w,
-		output reg signed[13:0] alpha,
-		output reg signed[13:0] beta,
+		input wire signed[NUM_BITS - 1 : 0] in_u,
+		input wire signed[NUM_BITS - 1 : 0] in_v,
+		input wire signed[NUM_BITS - 1 : 0] in_w,
+		output reg signed[NUM_BITS - 1 : 0] alpha,
+		output reg signed[NUM_BITS - 1 : 0] beta,
 		output reg done
 	);
 // alpha = (2/3) * (in_u - (1/2) * in_v - (1/2) * in_w)
@@ -14,31 +40,33 @@ module balanced_clarke(
 
 // alpha = (1/3) * (2 * in_u - in_v - in_w)
 // beta = (sqrt(3)/3) * (in_v - in_w)
+	localparam[63:0] SQRT3_BY_3 = 64'd5325116328314171701;	//2^63 * sqrt(3) / 3
+	localparam[63:0] ONE_THIRD = 64'h5555555555555555;	//2^64 / 3
 
 	reg[3:0] state;
-	reg signed[15:0] alpha_t1;
+	reg signed[NUM_BITS + 1 : 0] alpha_t1;
 	
-	reg signed[15:0] mul_in_a;
-	reg signed[15:0] mul_in_b;
-	reg signed[31:0] mul_res;
+	reg signed[NUM_BITS + 1 : 0] mul_in_a;
+	reg signed[NUM_BITS + 1 : 0] mul_in_b;
+	reg signed[2 * (NUM_BITS + 2) - 1 : 0] mul_res;
 	
-	parameter S_CALC1 = 4'h0;
-	parameter S_CALC2 = 4'h1;
-	parameter S_CLIP_BETA = 4'h2;
-	parameter S_CLIP_ALPHA = 4'h3;
+	localparam S_CALC1 = 4'h0;
+	localparam S_CALC2 = 4'h1;
+	localparam S_CLIP_BETA = 4'h2;
+	localparam S_CLIP_ALPHA = 4'h3;
 	
 	always @(posedge clk or posedge rst)
 	begin
 		if(rst)
 		begin
-			alpha <= 14'h0000;
-			beta <= 14'h0000;
+			alpha <= {NUM_BITS{1'b0}};
+			beta <= {NUM_BITS{1'b0}};
 			done <= 1'b0;
 			state <= S_CALC1;
-			alpha_t1 <= 16'h0000;
-			mul_in_a <= 16'h0000;
-			mul_in_b <= 16'h0000;
-			mul_res <= 32'h00000000;
+			alpha_t1 <= {(NUM_BITS + 2){1'b0}};
+			mul_in_a <= {(NUM_BITS + 2){1'b0}};
+			mul_in_b <= {(NUM_BITS + 2){1'b0}};
+			mul_res <= {(2 * (NUM_BITS + 2)){1'b0}};
 		end
 		else
 		begin
@@ -48,32 +76,32 @@ module balanced_clarke(
 				S_CALC1:
 				begin
 					done <= 1'b0;
-					alpha_t1 <= {in_u[13], in_u[13:0], 1'b0} - {{2{in_v[13]}}, in_v[13:0]};	// 2 * in_u - in_v
-					mul_in_a <= {{2{in_v[13]}}, in_v[13:0]} - {{2{in_w[13]}}, in_w[13:0]};	// in_v - in_w
-					mul_in_b <= 16'h49E7;	// 2^15 * sqrt(3)/3
+					alpha_t1 <= {in_u[NUM_BITS - 1], in_u[NUM_BITS - 1 : 0], 1'b0} - {{2{in_v[NUM_BITS - 1]}}, in_v[NUM_BITS - 1 : 0]};	// 2 * in_u - in_v
+					mul_in_a <= {{2{in_v[NUM_BITS - 1]}}, in_v[NUM_BITS - 1 : 0]} - {{2{in_w[NUM_BITS - 1]}}, in_w[NUM_BITS - 1 : 0]};	// in_v - in_w
+					mul_in_b <= SQRT3_BY_3[63 : 64 - NUM_BITS - 2];
 					if(trig)
 						state <= S_CALC2;
 				end
 				S_CALC2:
 				begin
-					mul_in_a <= alpha_t1 - {{2{in_w[13]}}, in_w[13:0]};	// 2 * in_u - in_v - in_w
-					mul_in_b <= 16'h5555;	// 2^16 * 1/3
+					mul_in_a <= alpha_t1 - {{2{in_w[NUM_BITS - 1]}}, in_w[NUM_BITS - 1 : 0]};	// 2 * in_u - in_v - in_w
+					mul_in_b <= ONE_THIRD[63 : 64 - NUM_BITS - 2];
 					state <= S_CLIP_BETA;
 				end
 				S_CLIP_BETA:
 				begin
-					if(&mul_res[30:28] || ~|mul_res[30:28])
-						beta <= mul_res[28:15];
+					if(&mul_res[NUM_BITS * 2 + 2 : NUM_BITS * 2] || ~|mul_res[NUM_BITS * 2 + 2 : NUM_BITS * 2])
+						beta <= mul_res[NUM_BITS * 2 : NUM_BITS + 1];
 					else
-						beta <= {mul_res[31], {13{~mul_res[31]}}};
+						beta <= {mul_res[2 * (NUM_BITS + 2) - 1], {(NUM_BITS - 1){~mul_res[2 * (NUM_BITS + 2) - 1]}}};
 					state <= S_CLIP_ALPHA;
 				end
 				S_CLIP_ALPHA:
 				begin
-					if(&mul_res[31:29] || ~|mul_res[31:29])
-						alpha <= mul_res[29:16];
+					if(&mul_res[NUM_BITS * 2 + 3 : NUM_BITS * 2 + 1] || ~|mul_res[NUM_BITS * 2 + 3 : NUM_BITS * 2 + 1])
+						alpha <= mul_res[NUM_BITS * 2 + 1 : NUM_BITS + 2];
 					else
-						alpha <= {mul_res[31], {13{~mul_res[31]}}};
+						alpha <= {mul_res[2 * (NUM_BITS + 2) - 1], {(NUM_BITS - 1){~mul_res[2 * (NUM_BITS + 2) - 1]}}};
 					done <= 1'b1;
 					state <= S_CALC1;
 				end
---
+++ Nano-FOC_LandShark_ADC_Bridge/src/tb_balanced_clarke_16.sv
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+module tb_balanced_clarke_16;
+	real PI;
+	real phase_u;
+	real phase_v;
+	real phase_w;
+	integer int_u;
+	integer int_v;
+	integer int_w;
+	reg[15:0] phi;
+	
+	assign PI = 3.14159265358979323846;
+	assign phase_u = $cos(($itor(phi) / 65536.0) * 2 * PI);
+	assign phase_v = $cos(($itor(phi) / 65536.0 - 1.0 / 3.0) * 2 * PI);
+	assign phase_w = $cos(($itor(phi) / 65536.0 + 1.0 / 3.0) * 2 * PI);
+	assign int_u = $rtoi(32767.0 * phase_u);
+	assign int_v = $rtoi(32767.0 * phase_v);
+	assign int_w = $rtoi(32767.0 * phase_w);
+	
+	reg clk_25;
+	reg rst;
+
+	wire signed[15:0] alpha;
+	wire signed[15:0] beta;
+	wire done;
+	
+	always @(posedge clk_25)
+	begin
+		if(rst)
+			phi <= 16'h0000;
+		else
+			phi <= phi + 16'h0001;
+	end
+
+//#############################################################################
+	balanced_clarke
+	#(
+		.NUM_BITS(16)
+	) balanced_clarke_i
+	(
+		.clk(clk_25),
+		.rst(rst),
+		.trig(1'b1),
+		.in_u(int_u[15:0]),
+		.in_v(int_v[15:0]),
+		.in_w(int_w[15:0]),
+		.alpha(alpha),
+		.beta(beta),
+		.done(done)
+	);
+//#############################################################################
+
+	always
+	begin
+		#20 clk_25 = ~clk_25;
+	end
+
+	initial
+	begin
+		// Initialize Inputs
+		clk_25 = 1'b0;
+		rst = 1'b1;
+
+		// Wait 100 ns for global reset to finish
+		#80;
+        
+		// Add stimulus here
+		rst = 1'b0;
+
+	end
+	
+endmodule
